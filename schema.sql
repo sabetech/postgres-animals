069de7e89ba64bade66bbb230b3f93bd34252c81
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-06-17 18:20:48 +0000

    Work in progress

--- schema.sql
+++ schema.sql
@@ -7,3 +7,27 @@ CREATE TABLE animals(
     weight_kg NUMERIC(5,2)
 );
 
+DROP TABLE animals;
+
+CREATE TABLE animals( 
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    date_of_birth date,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg NUMERIC(5,2),
+    species VARCHAR(250)
+);
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(190),
+    age INT
+);
+
+ALTER TABLE animals DROP COLUMN species
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(190)
+);
+
