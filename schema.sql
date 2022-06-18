30301ff0566607cc507ff5bf0865603bf115e1b8
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-06-18 10:25:06 +0000

    Further Updates to Animals with JOINS

--- schema.sql
+++ schema.sql
@@ -23,12 +23,32 @@ CREATE TABLE animals(
 CREATE TABLE owners (
     id INT GENERATED ALWAYS AS IDENTITY,
     full_name VARCHAR(190),
-    age INT
+    age INT,
+    PRIMARY KEY(id)
+
 );
 
 ALTER TABLE animals DROP COLUMN species
 CREATE TABLE species (
     id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(190)
+    name VARCHAR(190),
+    PRIMARY KEY(id)
 );
+
+ALTER TABLE animals 
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_animals_species
+FOREIGN KEY (species_id) 
+REFERENCES species (id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals 
+ADD CONSTRAINT fk_animal_owners
+FOREIGN KEY(owner_id) 
+REFERENCES owners(id);
+
+
 
